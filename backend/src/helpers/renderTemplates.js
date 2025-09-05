3d8208be71d402ff231084f33ddfe46c47696991
HamzaHacx1 <hamzatariq924@gmail.com>
2025-09-05 20:39:26 +0500

    FIEL FIX

--- backend/src/helpers/renderTemplates.js
+++ backend/src/helpers/renderTemplates.js
@@ -3,7 +3,13 @@ import path from "path";
 import fs from "fs";
 
 export function renderTemplate(fileName, data = {}) {
-  const filePath = path.join(process.cwd(), "src", "email-templates", fileName);
+  const filePath = path.join(
+    process.cwd(),
+    "backend",
+    "src",
+    "email-templates",
+    fileName
+  );
   const source = fs.readFileSync(filePath, "utf8");
   const template = Handlebars.compile(source);
   return template(data);
